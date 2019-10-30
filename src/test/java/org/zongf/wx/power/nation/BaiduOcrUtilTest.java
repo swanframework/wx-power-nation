58f96d76ef1a0e38acbdeb13c9611cab7e4a5d85
zonggaofeng <zonggaofeng@autohome.com>
2019-10-30 11:26:36 +0800

    新增百度ocr 工具测试类

--- src/test/java/org/zongf/wx/power/nation/BaiduOcrUtilTest.java
+++ src/test/java/org/zongf/wx/power/nation/BaiduOcrUtilTest.java
@@ -1,38 +1,54 @@
 package org.zongf.wx.power.nation;
 
+import org.junit.Test;
 import org.zongf.wx.power.nation.util.BaiduOcrUtil;
 import org.zongf.wx.power.nation.util.FileUtils;
 import org.zongf.wx.power.nation.vo.ocr.OcrResponse;
 import org.zongf.wx.power.nation.vo.ocr.TextArea;
 
-/**
+/** 百度ocr 工具类测试
  * @author: zongf
  * @created: 2019-10-24
  * @since 1.0
  */
 public class BaiduOcrUtilTest {
 
-    // 测试不包含位置信息接口
-    public void basicOcr() throws Exception{
+    private String imgPath = "F:\\study-app\\123.png";
 
-        String imgPath = "F:/images/0.png";
 
-        OcrResponse basicOcrResponse = BaiduOcrUtil.doBasicOcr(FileUtils.getFileBytes(imgPath));
+    // 测试通用文字基础解析, 自动去除头部
+    @Test
+    public void doBasicOcr() throws Exception{
 
+        OcrResponse basicOcrResponse = BaiduOcrUtil.doBasicOcr(FileUtils.getImageBytesWithoutHead(imgPath));
+
+        System.out.println("\n\n***********************************\n\n");
         for (TextArea textArea : basicOcrResponse.getWords_result()) {
             System.out.println(textArea.getWords());
         }
     }
 
-    // 测试包含位置信息接口
-    public void locationOcr() throws Exception{
+    // 测试通用文字高精度解析, 自动去除头部
+    @Test
+    public void doBasicAccurateOcr() throws Exception{
 
-        String imgPath = "/home/zongf/Desktop/imgs/1.jpg";
+        OcrResponse basicOcrResponse = BaiduOcrUtil.doBasicAccurateOcr(FileUtils.getImageBytesWithoutHead(imgPath));
 
-        OcrResponse basicOcrResponse = BaiduOcrUtil.doBasicAccurateOcr(FileUtils.getFileBytes(imgPath));
-
+        System.out.println("\n\n***********************************\n\n");
         for (TextArea textArea : basicOcrResponse.getWords_result()) {
             System.out.println(textArea.getLocation() + " --> " + textArea.getWords());
+        }
+    }
+
+    // 测试通用文字基础解析, 不去除头部
+    @Test
+    public void doBasicOcrWithHead() throws Exception{
+
+        OcrResponse basicOcrResponse = BaiduOcrUtil.doBasicOcr(FileUtils.getFileBytes(imgPath));
+
+        System.out.println("\n\n***********************************\n\n");
+        for (TextArea textArea : basicOcrResponse.getWords_result()) {
+            System.out.println(textArea.getWords());
         }
     }
 
