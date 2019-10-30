7e058f5be3971f577a965262e68dc3140aa67c58
zonggaofeng <zonggaofeng@autohome.com>
2019-10-30 19:25:51 +0800

    更新sql

--- sql/db.sql
+++ sql/db.sql
@@ -4,7 +4,7 @@ create table `study-app`.s_image
 	content longblob null,
 	category char null,
 	basicOcr varchar(3000) null,
-	locOcr varchar(3000) null,
+	accurateOcr varchar(3000) null,
 	status char null,
 	createTime datetime null,
 	constraint s_image_id_uindex
@@ -19,14 +19,17 @@ create table `study-app`.s_question
 	id int auto_increment,
 	imageId int not null,
 	title varchar(300) null,
+	titleLines int null,
 	answer varchar(100) null,
 	answerIdx int null,
-	answerLoc varchar(1000) null,
 	createTime datetime null,
+	options varchar(1000) null,
 	constraint s_question_id_uindex
 		unique (id)
 );
 
 alter table `study-app`.s_question
 	add primary key (id);
+
+
 
